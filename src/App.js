302fba6dfa56344a3729554e5fd096d3d0bed275
saurabh_bakolia <skbakolia2002@gmail.com>
2022-12-08 00:22:36 +0530

    LandingPage done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
+import SkillsPage from './components/SkillsPage'
+import PortfolioPage from './components/PortfolioPage'
+import ExperiencePage from './components/ExperiencePage'
+import ContactPage from './components/ContactPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/skills" element={<SkillsPage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/exp" element={<ExperiencePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
+    // </main>
+
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <navbar className="flex justify-around items-center h-[10vh]">
+            <div>
+                <h1 className="font-extrabold text-xl text-gray-600 tracking-wide">SCROLL<span className="text-[#16B0B2]">ME</span></h1>
+            </div>
+            <div className="w-[38%] text-base text-white font-semibold tracking-wider">
+                <ul className="flex justify-evenly">
+                    <li className="cursor-pointer"><Link to='/'>Home</Link></li>
+                    <li className="cursor-pointer"><Link to='/about'>About</Link></li>
+                    <li className="cursor-pointer"><Link to='/skills'>Skills</Link></li>
+                    <li className="cursor-pointer"><Link to='/portfolio'>Portfolio</Link></li>
+                    <li className="cursor-pointer"><Link to='/exp'>Experience</Link></li>
+                    <li className="cursor-pointer"><Link to='/contact'>Contact</Link></li>
+                </ul>
+            </div>
+        </navbar>
+    );
+};
+
+export default Navbar;
+
+//? #16B0B2
