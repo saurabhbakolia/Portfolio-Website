246c244df98ef85d21adc3752383898351a10b42
saurabh bakolia <skbakolia2002@gmail.com>
2023-01-15 00:45:34 +0530

    menu hover effect duration changed to 300

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,12 +6,12 @@ const Menu = ({ isOpen, setOpen }) => {
         <div className='hidden md:flex justify-between items-center'>
             <div className="md:block text-base text-gray-600 font-semibold tracking-wider">
                 <ul className="flex justify-evenly items-center tracking-wide space-x-4">
-                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-500 ease-in-out"><Link to='/'>Home</Link></li>
-                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-500 ease-in-out"><Link to='/about'>About</Link></li>
-                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-500 ease-in-out"><Link to='/skills'>Skills</Link></li>
-                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-500 ease-in-out"><Link to='/portfolio'>Portfolio</Link></li>
-                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-500 ease-in-out"><Link to='/exp'>Experience</Link></li>
-                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-500 ease-in-out"><Link to='/contact'>Contact</Link></li>
+                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-300 ease-in-out"><Link to='/'>Home</Link></li>
+                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-300 ease-in-out"><Link to='/about'>About</Link></li>
+                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-300 ease-in-out"><Link to='/skills'>Skills</Link></li>
+                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-300 ease-in-out"><Link to='/portfolio'>Portfolio</Link></li>
+                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-300 ease-in-out"><Link to='/exp'>Experience</Link></li>
+                    <li className="cursor-pointer hover:text-[#16B0B2] transition-all duration-300 ease-in-out"><Link to='/contact'>Contact</Link></li>
                 </ul>
             </div>
         </div>
