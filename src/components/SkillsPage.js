fc2867d5a73ac4a27a3e5b0ec0c3d0dd35bc2943
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2023-09-22 20:20:48 +0530

    Skills Card added

--- src/components/SkillsPage.js
+++ src/components/SkillsPage.js
@@ -18,8 +18,8 @@ const SkillsPage = () => {
                         <SocialMediaIcons />
                     </div>
                     <div className="text-gray-600 flex flex-col justify-center items-center md:items-start w-full h-full lg:h-[89.90vh] lg:mr-[15.5%] bg-[#DFF7F8] gap-6 md:gap-4 py-6 md:p-4 overflow-y-auto ">
-                        <h1 className="xl:pt-2 h-fit text-3xl text-[#16B0B2] font-medium tracking-wider border-b-4 border-[#16B0B2]">Skills</h1>
-                        <div className="">
+                        <h1 className="pt-16 text-3xl text-[#16B0B2] font-medium tracking-wider border-b-4 border-[#16B0B2]">Skills</h1>
+                        <div className="w-full grid grid-cols-4 grid-flow-row place-content-center place-items-center gap-4">
                             {skills.map((skill) => (
                                 <SkillCard id={skill.id} skill={skill.skills} icon={skill.icon} />
                             ))}
