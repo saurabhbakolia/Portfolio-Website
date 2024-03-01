d1ed5efd7c8771edc0004a1bff275793e6de7279
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2024-03-01 20:26:26 +0530

    logo is now clickable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,14 @@ import React from "react";
 import Menu from './Menu/Menu'
 import Burger from './Menu/Burger'
 import Portal from "./PortalComponent/PortalComponent";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className="navbar flex justify-center items-center">
             <div className="sticky top-0 right-0 w-[90vw] lg:w-[80vw] flex justify-between items-start md:items-center py-[2.5vh] h-[10vh]">
                 <div className="">
-                    <h1 className="font-bold text-2xl text-gray-600 tracking-wider cursor-pointer">SCROLL<span className="font-bold text-[#16B0B2]">ME</span></h1>
+                    <Link to={"/"}> <h1 className="font-bold text-2xl text-gray-600 tracking-wider cursor-pointer">SCROLL<span className="font-bold text-[#16B0B2]">ME</span></h1></Link>
                 </div>
                 <div className="flex items-center relative justify-center">
                     <Menu />
