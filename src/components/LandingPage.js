82a325d56852843ef5dbf2a50ecbf9c788068a4a
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2024-03-01 20:06:11 +0530

    type writer effect added

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,6 +4,18 @@ import SocialMediaIcons from './SocialMediaIcons';
 import { Link } from 'react-router-dom';
 import Image from '../assets/code.jpg';
 import StyledButton from './CustomComponent/StyledButton';
+import Typewriter from "typewriter-effect";
+import TypewriterComponent from './TypeWritorEffect/TypewriterComponent';
+
+const TypewriterEffect = ({ text }) => {
+    const { currentText } = Typewriter({
+        text,
+        loop: true, // Set to true if you want the typewriter effect to loop
+        delay: 100, // Delay between each character in milliseconds
+    });
+
+    return <span>{currentText}</span>;
+};
 
 const LandingPage = () => {
     return (
@@ -22,7 +34,8 @@ const LandingPage = () => {
                             Saurabh Bakolia
                         </h1>
                         <p className="text-gray-500 font-semibold tracking-wider md:text-2xl lg:text-xl">
-                            Full Stack Web Developer
+                            {/* Full Stack Web Developer */}
+                            <TypewriterComponent/>  
                         </p>
                         <p className="tracking-normal text-sm md:text-lg text-gray-600">
                             Designing, developing, and maintaining fully-fledged and
