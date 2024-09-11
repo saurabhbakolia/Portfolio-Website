b51ce3ecd58bf5b815c48eb14536af17715fd00a
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2024-09-11 10:07:33 +0530

    feat(SkillsPage): add drag-and-drop functionality to rearrange skills

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,18 +1,39 @@
-import { useTheme } from '@emotion/react'
-import React, { useContext } from 'react'
-import ThemeContext from '../contexts/ThemeContext'
+import React, { useContext } from "react";
+import ThemeContext from "../contexts/ThemeContext";
+import { motion } from "framer-motion";
 
 const SkillCard = (props) => {
-    const { theme } = useContext(ThemeContext);
-    return (
-        <div className={`skill-card ${theme === 'dark' ? 'dark-mode-card-bg dark-mode-white-text' : 'bg-[#16afb218]'}`} key={props.id} >
-            <div className='h-fit flex justify-center items-center'>
-                {props.icon}
-            </div>
-            <p className={`text-center mt-2 text-xl  hidden sm:block ${theme === 'dark' ? 'dark-mode-white-text' : 'text-gray-700'}`}>{props.skill}</p>
-        </div>
-    )
-}
+	const { theme } = useContext(ThemeContext);
+	const {
+		onDragStart,
+		onDragOver,
+		onDrop,
+		dropIndicator,
+		onDragEnd,
+		isDragging,
+	} = props;
+	return (
+		<motion.div
+			className={`skill-card ${theme === "dark" ? "dark-mode-card-bg dark-mode-white-text" : "bg-[#16afb218]"} ${dropIndicator === props.id ? "bg-blue-200" : ""} ${isDragging ? "dragging" : ""}`}
+			key={props.id}
+			draggable
+			onDragStart={(event) => onDragStart(event, props.id)}
+			onDragOver={(event) => onDragOver(event, props.id)}
+			onDrop={(event) => onDrop(event, props.id)}
+			onDragEnd={onDragEnd}
+			initial={{ opacity: 0, scale: 0.9 }} // Initial state of the card
+			animate={{ opacity: 1, scale: 1 }} // Animated state of the card
+			transition={{ duration: 0.4, ease: "easeInOut" }} // Smooth transition settings
+			whileHover={{ scale: 1.05, boxShadow: "0px 10px 15px rgba(0,0,0,0.2)" }} // Scale effect and shadow on hover
+		>
+			<div className="h-fit flex justify-center items-center">{props.icon}</div>
+			<p
+				className={`text-center mt-2 text-xl  hidden sm:block ${theme === "dark" ? "dark-mode-white-text" : "text-gray-700"}`}
+			>
+				{props.skill}
+			</p>
+		</motion.div>
+	);
+};
 
-export default SkillCard
-
+export default SkillCard;
--- src/components/SkillsPage.js
+++ src/components/SkillsPage.js
@@ -1,74 +1,122 @@
-import React, { useContext } from 'react';
-import SocialMediaIcons from './SocialMediaIcons';
-import { FaLaptopCode } from 'react-icons/fa';
-import { GoTools } from 'react-icons/go';
-import SkillsProgress from './SkillsProgress';
-import skills from '../data/skills';
-import technologies from '../data/technologies';
-import SkillCard from './SkillCard';
-import { motion } from 'framer-motion';
-import Tilt from 'react-parallax-tilt';
-import { useTheme } from '@emotion/react';
-import ThemeContext from '../contexts/ThemeContext';
-
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import skills from "../data/skills";
+import SkillCard from "./SkillCard";
+import { motion } from "framer-motion";
+import Tilt from "react-parallax-tilt";
+import ThemeContext from "../contexts/ThemeContext";
 
 const container = {
-    hidden: { opacity: 1, scale: 0 },
-    visible: {
-        opacity: 1,
-        scale: 1,
-        transition: {
-            delayChildren: 0.3,
-            staggerChildren: 0.2,
-        },
-    },
+	hidden: { opacity: 1, scale: 0 },
+	visible: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			delayChildren: 0.3,
+			staggerChildren: 0.2,
+		},
+	},
 };
-
-const item = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-        y: 0,
-        opacity: 1,
-    },
-};
-
 
 const SkillsPage = () => {
-    const { theme } = useContext(ThemeContext);
-    return (
-        <main className="skill-page-main">
-            <div className={`skill-page-box ${theme === 'dark' ? '' : 'bg-[#DFF7F8]'} gap-6 md:gap-4 md:p-4`}>
-                <h1 className={`text-3xl font-medium tracking-wider border-b-4 ${theme === 'dark' ? 'dark-mode-white-text border-[#1A8B9D]' : 'border-[#16B0B2]'}`}>Skills</h1>
-                <div className="w-full overflow-y-scroll mt-4 py-8 px-4 scroll-smooth">
-                    <motion.ui
-                        className="flex flex-wrap flex-row justify-between items-center gap-4"
-                        variants={container}
-                        initial="hidden"
-                        animate="visible"
-                    >
-                        {skills.map((skill) => (
-                            <Tilt
-                                tiltEnable={true}
-                                tiltReverse={true}
-                                tiltMaxAngleX={15}
-                                tiltMaxAngleY={15}
-                                scale={1.05}
-                                transitionSpeed={2500}
-                                glareEnable={true}
-                                glareMaxOpacity={0.75}
-                                glarePosition="bottom"
-                                glareColor="#ffffff"
-                                glareBorderRadius="30px"
-                                perspective={1000}
-                            >
-                                <SkillCard id={skill.id} skill={skill.skills} icon={skill.icon} />
-                            </Tilt>
-                        ))}
-                    </motion.ui>
-                </div>
-            </div>
-        </main>
-    );
+	const { theme } = useContext(ThemeContext);
+	const [skillsItems, setSkillsItems] = useState(skills);
+	const [draggedSkillItem, setDraggedSkillItem] = useState(null);
+	const [dropIndicator, setDropIndicator] = useState(null); // To highlight the drop area
+
+	const handleDragStart = useCallback((event, id) => {
+		event.dataTransfer.setData("text/plain", id);
+		setDraggedSkillItem(id);
+	}, []);
+
+	const handleDragEnd = () => {
+		setDraggedSkillItem(null); // Reset after drag ends
+		setDropIndicator(null); // Reset the drop indicator
+	};
+
+	const handleDragOver = useCallback((event, skillId) => {
+		event.preventDefault(); // Necessary to allow dropping
+		setDraggedSkillItem(null);
+		setDropIndicator(skillId);
+	}, []);
+
+	const handleDrop = useCallback(
+		(event, id) => {
+			event.preventDefault();
+			const draggedId = event.dataTransfer.getData("text/plain");
+
+			if (draggedId === id) return; // Prevent dropping the same item on itself
+
+			const draggedIndex = skillsItems.findIndex(
+				(skill) => +skill.id === +draggedId,
+			);
+			const droppedIndex = skillsItems.findIndex((skill) => +skill.id === +id);
+
+			if (draggedIndex === -1 || droppedIndex === -1) {
+				return; // Handle the case where indices are not found
+			}
+			// Swap items in the array
+			const updatedSkills = [...skillsItems];
+			const [movedSkill] = updatedSkills.splice(draggedIndex, 1); // Remove dragged item
+			updatedSkills.splice(droppedIndex, 0, movedSkill); // Insert it at the new position
+
+			setSkillsItems(updatedSkills);
+			setDraggedSkillItem(null);
+			setDropIndicator(null); // Clear the drop indicator after the drop
+		},
+		[skillsItems],
+	);
+
+	return (
+		<main className="skill-page-main">
+			<div
+				className={`skill-page-box ${theme === "dark" ? "" : "bg-[#DFF7F8]"} gap-6 md:gap-4 md:p-4`}
+			>
+				<h1
+					className={`text-3xl font-medium tracking-wider border-b-4 ${theme === "dark" ? "dark-mode-white-text border-[#1A8B9D]" : "border-[#16B0B2]"}`}
+				>
+					Skills
+				</h1>
+				<div className="w-full overflow-y-scroll mt-4 py-8 px-4 scroll-smooth">
+					<motion.ui
+						className="flex flex-wrap flex-row justify-between items-center gap-4"
+						variants={container}
+						initial="hidden"
+						animate="visible"
+					>
+						{skillsItems.map((skill) => (
+							<Tilt
+								key={skill.id}
+								tiltEnable={true}
+								tiltReverse={true}
+								tiltMaxAngleX={15}
+								tiltMaxAngleY={15}
+								scale={1.05}
+								transitionSpeed={2500}
+								glareEnable={true}
+								glareMaxOpacity={0.75}
+								glarePosition="bottom"
+								glareColor="#ffffff"
+								glareBorderRadius="30px"
+								perspective={1000}
+							>
+								<SkillCard
+									id={skill.id}
+									skill={skill.skills}
+									icon={skill.icon}
+									onDragStart={handleDragStart}
+									onDragOver={handleDragOver}
+									onDrop={handleDrop}
+									dropIndicator={dropIndicator}
+									onDragEnd={handleDragEnd}
+									isDragging={draggedSkillItem === skill.id}
+								/>
+							</Tilt>
+						))}
+					</motion.ui>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default SkillsPage;
